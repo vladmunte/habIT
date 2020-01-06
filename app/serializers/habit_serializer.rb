ea9f04fc04b667be7210cde1a33e5f87f027c67a
Vlad Munte <vlad.munte@luminos.software>
2020-01-06 16:11:06 +0200

    add condition to habit progress

--- app/serializers/habit_serializer.rb
+++ app/serializers/habit_serializer.rb
@@ -15,8 +15,7 @@ class HabitSerializer < ActiveModel::Serializer
   attribute :habit_progress
 
   def habit_progress
+    return 1 if 100*object.days_checked/object.days_goal < 1
     return 100*object.days_checked/object.days_goal
   end
-
-
 end
